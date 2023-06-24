4d7597f8e66fd5fb71cd5c40c2ba740953636db3
thiagotask <thtasca@gmail.com>
2023-06-24 08:42:46 -0300

    Delete core directory

--- core/footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer" class="container">
-   <div class="section">
-      <p>PUBG, PLAYERUNKNOWN’S BATTLEGROUNDS and all related logos and images are trademarks of PUBG Corporation or its affiliates.</p>
-      <p class="made">Made by thtasca</p>
-   </div>
-</div>
-
-<script src="assets/script.js" > </script>
